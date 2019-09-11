756781aceac4e536402bf7cf635e3f5d2c180dce
Matthieu Le brazidec <matthieu@lebrazidec.email>
2019-09-11 23:36:19 +0200

    Added a with_limit and with_revision methods to GetRequest

--- src/kv/get.rs
+++ src/kv/get.rs
@@ -5,6 +5,8 @@ use crate::ResponseHeader;
 pub struct GetRequest {
     key: String,
     end_key: Option<Vec<u8>>,
+    limit: i64,
+    revision: i64,
     serializable: bool,
     keys_only: bool,
     count_only: bool,
@@ -18,6 +20,8 @@ impl GetRequest {
         Self {
             key: key.into(),
             end_key: None,
+            limit: 0,
+            revision: 0,
             serializable: false,
             keys_only: false,
             count_only: false,
@@ -42,10 +46,13 @@ impl GetRequest {
 
             end
         };
+
         Self {
             key: key,
             end_key: Some(end_key),
-            serializable: false,
+	        limit: 0,
+	        revision: 0,
+	        serializable: false,
             keys_only: false,
             count_only: false,
         }
@@ -58,11 +65,23 @@ impl GetRequest {
         Self {
             key: key.into(),
             end_key: Some(end_key.into().into_bytes()),
-            serializable: false,
+	        limit: 0,
+	        revision: 0,
+	        serializable: false,
             keys_only: false,
             count_only: false,
         }
     }
+
+	pub fn with_limit(mut self, limit: i64) -> Self {
+		self.limit = limit;
+		self
+	}
+
+	pub fn with_revision(mut self, revision: i64) -> Self {
+		self.revision = revision;
+		self
+	}
 
     pub fn with_serializable(mut self) -> Self {
         self.serializable = true;
@@ -88,6 +107,9 @@ impl Into<rpc::RangeRequest> for GetRequest {
         if let Some(range_end) = self.end_key {
             req.set_range_end(range_end);
         }
+
+	    req.set_limit(self.limit);
+	    req.set_revision(self.revision);
         req.set_keys_only(self.keys_only);
         req.set_count_only(self.count_only);
         req.set_serializable(self.serializable);
