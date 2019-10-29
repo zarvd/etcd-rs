bf790d381a8957013ab69b4fb6fb192c221bd04c
ccc13 <currantxx@gmail.com>
2019-10-29 13:23:25 +0800

    Implement range key-value

--- examples/kv.rs
+++ examples/kv.rs
@@ -1,15 +1,86 @@
+use etcd_rs::*;
+
+async fn list_prefix(client: &Client) -> Result<()> {
+    println!("List key value pairs with prefix");
+
+    let prefix = "42_";
+
+    {
+        // Put some key-value pairs
+        client.kv().put(PutRequest::new("42_foo1", "baz1")).await?;
+        client.kv().put(PutRequest::new("42_foo2", "baz2")).await?;
+        client.kv().put(PutRequest::new("42_bar1", "baz3")).await?;
+        client.kv().put(PutRequest::new("42_bar2", "baz4")).await?;
+    }
+
+    {
+        // List key-value pairs with prefix
+        let req = RangeRequest::prefix(prefix);
+        let resp = client.kv().range(req).await?;
+        println!("Range Response: {:?}", resp);
+    }
+
+    Ok(())
+}
+
+async fn list_all(client: &Client) -> Result<()> {
+    println!("List all key value pairs");
+    {
+        // Put some key-value pairs
+        client.kv().put(PutRequest::new("foo1", "baz1")).await?;
+        client.kv().put(PutRequest::new("foo2", "baz2")).await?;
+        client.kv().put(PutRequest::new("bar1", "baz3")).await?;
+        client.kv().put(PutRequest::new("bar2", "baz4")).await?;
+    }
+
+    {
+        // List all key-value pairs
+        let req = {
+            let mut req = RangeRequest::all();
+            req.set_limit(4);  // Only returns 4 key-value pairs
+            req
+        };
+        let resp = client.kv().range(req).await?;
+        println!("Range Response: {:?}", resp);
+    }
+
+    Ok(())
+}
+
+async fn put_and_get(client: &Client) -> Result<()> {
+    println!("Put and get a key value pairs");
+
+    let key = "foo";
+    let value = "bar";
+
+    {
+        // Put a key-value pair
+        let req = PutRequest::new(key, value);
+        let resp = client.kv().put(req).await?;
+
+        println!("Put Response: {:?}", resp);
+    }
+
+    {
+        // Get the key-value pair
+        let req = RangeRequest::get(key);
+        let resp = client.kv().range(req).await?;
+        println!("Range Response: {:?}", resp);
+    }
+
+    Ok(())
+}
+
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let client = etcd_rs::Client::new(etcd_rs::ClientConfig {
+async fn main() -> Result<()> {
+    let client = Client::new(ClientConfig {
         endpoints: vec!["http://127.0.0.1:2379".to_owned()],
         auth: None,
     });
 
-    let req = etcd_rs::PutRequest::new("foo", "bar");
-
-    let resp = client.kv().put(req).await?;
-
-    println!("{:?}", resp);
+    put_and_get(&client).await?;
+    list_all(&client).await?;
+    list_prefix(&client).await?;
 
     Ok(())
 }
